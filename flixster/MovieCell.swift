e951afeedd0a151733503798d130c40063547fa5
Khalen Hudson <kphudson13@students.desu.edu@khalens-mbp.fios-router.home>
2019-01-29 21:53:01 -0500

    Un-finished assignment 1

---
+++ flixster/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  flixster
+//
+//  Created by Khalen Hudson on 1/29/19.
+//  Copyright © 2019 Khalen Hudson. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ flixster/MoviesViewController.swift
@@ -0,0 +1,70 @@
+//
+//  MoviesViewController.swift
+//  flixster
+//
+//  Created by Khalen Hudson on 1/29/19.
+//  Copyright © 2019 Khalen Hudson. All rights reserved.
+//
+
+import UIKit
+
+class MoviesViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var movies = [[String:Any]]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        print("Hello")
+        
+        // Network request snippet
+        let url = URL(string: "https://api.tumblr.com/v2/blog/humansofnewyork.tumblr.com/posts/photo?api_key=Q6vHoaVm5L1u2ZAW1fqv3Jw48gFzYVg9P0vH0VHl3GVy6quoGV")!
+        let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
+        session.configuration.requestCachePolicy = .reloadIgnoringLocalCacheData
+        let task = session.dataTask(with: url) { (data, response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+            } else if let data = data,
+                let dataDictionary = try! JSONSerialization.jsonObject(with: data, options: []) as? [String: Any] {
+                
+                self.movies = dataDictionary["results"] as! [[String:Any]]
+                self.tableView.reloadData()
+                print(dataDictionary)
+                
+                // TODO: Get the posts and store in posts property
+                
+                // TODO: Reload the table view
+            }
+        }
+        task.resume()        // Do any additional setup after loading the view.
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return movies.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        
+        let movie = movies[indexPath.row]
+        let title = movie["title"] as! String
+        
+        cell.textLabel!.text = title
+        return cell
+        
+    }
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
